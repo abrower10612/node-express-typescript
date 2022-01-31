5b7ebaa10e377af3d4b147e57405cd3eb1fcb8b0
Andrew Brower <63933741+abrower10612@users.noreply.github.com>
2022-01-30 21:15:17 -0700

    run build script

---
+++ dist/server.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
+const user_1 = __importDefault(require("./routes/user"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use('/user', user_1.default);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`listening on port ${port} 🔥`));
