2615cf1126180046bf5f9dd1f01d72f075eefee4
Andrew Brower <63933741+abrower10612@users.noreply.github.com>
2022-01-30 21:10:29 -0700

    add User interface to user model ts file

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,16 @@
 import { Request, Response } from 'express';
+import { User } from '../models/user';
 
 export const createUser = async (req: Request, res: Response) => {
-    res.send('create user works');
+    const { username, password } = req.body;
+
+    if (!username) return res.send('Missing username');
+    if (!password) return res.send('Missing password');
+
+    const newUser: User = {
+        username: username,
+        password: password
+    }
+
+    res.send(`Account successfully created for ${newUser.username}.`);
 }
